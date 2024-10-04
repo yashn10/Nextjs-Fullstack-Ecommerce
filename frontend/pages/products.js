398c2fc7c622225258edd1687312bcae60ed6606
Yash Nishankar <yashnishankar3102001@gmail.com>
2024-10-04 14:45:35 +0530

    search functionality added

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -7,8 +7,11 @@ const products = () => {
 
     const [data, setData] = useState([]);
     const [images, setImages] = useState({});
+    const [query, setquery] = useState('');
+    const [products, setproducts] = useState([]);
 
     const client = createClient('JepWJqaBJwRFZQ6EZJnBadthVBCAW2NZzwUUjUn2RsqYfD6EVwFGJ8cF');
+
 
     const fetchPhotos = async (query) => {
         try {
@@ -19,6 +22,7 @@ const products = () => {
             return 'https://via.placeholder.com/400x300';
         }
     };
+
 
     const fetchData = async () => {
         try {
@@ -47,42 +51,84 @@ const products = () => {
     }, []);
 
 
+    useEffect(() => {
+        const filteredproducts = query ?
+            data.filter((product) => product.name.toLowerCase().includes(query.toLowerCase())) : data;
+
+        setproducts(filteredproducts);
+    }, [query, data]);
+
+
     return (
 
         <div className="row py-12 px-4 md:px-8">
 
-            <div className="flex flex-wrap -m-4">
-                {data.map((product) => (
-                    <div className="p-4 lg:w-1/4 md:w-1/2 sm:w-1/2" key={product._id}>
-                        <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105">
-                            <div className="relative">
-                                <img
-                                    src={images[product._id]}
-                                    alt={product.name}
-                                    className="w-full h-48 object-cover transition-opacity duration-300 hover:opacity-80"
-                                />
-                                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30">
-                                    <span className="text-white text-lg font-semibold">{product.name}</span>
+            {/* Updated Search Bar */}
+            <div className="input-field" style={{ position: 'relative', width: '25%' }}>
+                <input
+                    type="text"
+                    id="search"
+                    placeholder="Search for a product..."
+                    className="black-text"
+                    value={query}
+                    onChange={(e) => setquery(e.target.value)}
+                    style={{
+                        paddingLeft: '2.5rem', // Adjust padding for the icon
+                        backgroundColor: 'white', // White background
+                        borderRadius: '25px', // Rounded edges
+                        color: 'black',
+                    }}
+                />
+                {/* Search Icon */}
+                <i
+                    className="material-icons prefix grey-text text-darken-1"
+                    style={{
+                        position: 'absolute',
+                        left: '0.5rem',
+                        top: '0.9rem',
+                    }}
+                >
+                    search
+                </i>
+            </div>
+
+
+            <div className="flex flex-wrap">
+                {products.length > 0 ? (
+                    products.map((product) => (
+                        <div className="p-4 lg:w-1/4 md:w-1/2 sm:w-1/2" key={product._id}>
+                            <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105">
+                                <div className="relative">
+                                    <img
+                                        src={images[product._id]}
+                                        alt={product.name}
+                                        className="w-full h-48 object-cover transition-opacity duration-300 hover:opacity-80"
+                                    />
+                                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30">
+                                        <span className="text-white text-lg font-semibold">{product.name}</span>
+                                    </div>
+                                </div>
+                                <div className="p-4">
+                                    <p className="text-gray-700 mb-3">{product.desc}</p>
+                                </div>
+                                <div className="p-4 border-t border-gray-200">
+                                    <Link
+                                        href={`/product/${product._id}?image=${encodeURIComponent(images[product._id])}`}
+                                        className="text-red-500 inline-flex items-center transition-colors duration-300 hover:text-red-700"
+                                    >
+                                        View details
+                                        <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                            <path d="M5 12h14"></path>
+                                            <path d="M12 5l7 7-7 7"></path>
+                                        </svg>
+                                    </Link>
                                 </div>
                             </div>
-                            <div className="p-4">
-                                <p className="text-gray-700 mb-3">{product.desc}</p>
-                            </div>
-                            <div className="p-4 border-t border-gray-200">
-                                <Link
-                                    href={`/product/${product._id}?image=${encodeURIComponent(images[product._id])}`}
-                                    className="text-red-500 inline-flex items-center transition-colors duration-300 hover:text-red-700"
-                                >
-                                    View details
-                                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                        <path d="M5 12h14"></path>
-                                        <path d="M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </Link>
-                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))
+                ) : (
+                    <h1 className="w-full text-center" style={{ fontSize: "30px", height: "100vh", alignContent: "center" }}>No products available !!</h1>
+                )}
             </div>
 
         </div>
